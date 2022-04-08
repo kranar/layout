f76f795bce6478bda5c90a65c7b00c38c44e1715
Kamal Mansouri <kamal@eidolonsystems.com>
2022-04-08 00:31:01 -0400

    Added unit tests to expand an expression.

--- tests/library_tests/manipulations_tester.py
+++ tests/library_tests/manipulations_tester.py
@@ -3,6 +3,11 @@ import unittest
 from library import *
 
 
+x = VariableExpression('x')
+y = VariableExpression('y')
+z = VariableExpression('z')
+
+
 class ManipulationsTester(unittest.TestCase):
   def test_expanding_literal(self):
     expression = LiteralExpression(5)
@@ -10,12 +15,14 @@ class ManipulationsTester(unittest.TestCase):
     self.assertEqual(expansion, expression)
 
   def test_distribute_multiplication_addition(self):
-    x = VariableExpression('x')
-    y = VariableExpression('y')
-    z = VariableExpression('z')
     expression = x * (y + z)
     expansion = expand(expression)
     self.assertEqual(expansion, x * y + x * z)
+
+  def test_distribute_multiplication_subtraction(self):
+    expression = x * (y - z)
+    expansion = expand(expression)
+    self.assertEqual(expansion, x * y - x * z)
 
 
 if __name__ == '__main__':

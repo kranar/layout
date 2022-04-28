8bc6f9676a933442a4b3532077b05c2f9887cb8a
Kamal <kamal@eidolonsystems.com>
2022-04-27 21:48:10 -0400

    Work started to unit test only tests whose names match a pattern.

--- tests/library_tests/test_suite.py
+++ tests/library_tests/test_suite.py
@@ -5,11 +5,20 @@ from tests.library_tests.manipulations_tester import ManipulationsTester
 from tests.library_tests.solver_tester import SolverTester
 
 
+def make_suite(type, pattern):
+  loader = unittest.TestLoader()
+  loader.sortTestMethodsUsing = unittest.util.three_way_cmp
+  loader.testMethodPrefix = 'test'
+  loader.testNamePatterns = pattern
+  loader.suiteClass = unittest.suite.TestSuite
+  return loader.loadTestsFromTestCase(type)
+
+
 def suite():
   suite = unittest.TestSuite()
-  suite.addTest(unittest.makeSuite(ManipulationsTester))
-  suite.addTest(unittest.makeSuite(SolverTester))
-  suite.addTest(unittest.makeSuite(LayoutTester))
+  suite.addTest(make_suite(ManipulationsTester, ['asdasd']))
+  suite.addTest(make_suite(SolverTester, 'solve_single_variable'))
+  suite.addTest(make_suite(LayoutTester, 'asdasdasd'))
   return suite
 
 
